5ebb792bd2501afa7071246358465e10d609133a
ganeshunhale <ganeshunhale.15@gmail.com>
2024-12-12 15:27:26 +0530

    fix: bug

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let level = 0;
 let started = false;
 let yourScore = 0
 let highestScore = localStorage.getItem('highScore') || 0
-console.log(highestScore)
+let startBtn = document.getElementById('start-btn')
+let levelTitle = document.getElementById("level-title");
 
 document.addEventListener("keydown", () => {
     if (!started) {
@@ -15,25 +16,40 @@ document.addEventListener("keydown", () => {
     }
 });
 
+function getDeviceType() {
+    const userAgent = navigator.userAgent.toLowerCase();
+    if (/mobile|android|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent)) {
+        return 'Mobile';
+    }
+    if (/tablet|ipad/i.test(userAgent)) {
+        return 'Tablet';
+    }
+    return 'Desktop';
+}
 
 
 let yourScoreElement = document.getElementById('your-score-span')
 let highScoreElement = document.getElementById('high-score-span')
 let crownEmoji = document.getElementById('crown-emoji')
 highScoreElement.innerHTML = highestScore
-document.getElementById("level-title").addEventListener("click", (e) => {
-    console.log("clicled");
-    if (!started) {
-    nextSequence();
-        started = true;}
-        
-})
+if (getDeviceType() === "Mobile") {
+    levelTitle.style.display = 'none'
+    startBtn.setAttribute("style", "display: block !important;")
+    startBtn.addEventListener("click", (e) => {
+        console.log("clicled");
+        if (!started) {
+            nextSequence();
+            started = true;
+        }
+
+    })
+}
+
 
 document.querySelectorAll("[role='button']").forEach((btn) => {
     btn.addEventListener("click", (e) => {
         const userColor = e.target.id;
         userPattern.push(userColor);
-        // playSound(userColor);
         animatePress(userColor);
         checkAnswer(userPattern.length - 1);
     });
@@ -50,12 +66,12 @@ function checkAnswer(currentLevel) {
 
             yourScore = yourScore + 1;
             yourScoreElement.innerText = yourScore
-            console.log("highscoreafter",highestScore);
+            console.log("highscoreafter", highestScore);
             highestScore = localStorage.getItem('highScore') || 0
-            
+
 
             if (yourScore > highestScore) {
-                
+
                 highScoreElement.innerHTML = yourScore;
                 crownEmoji.style.display = 'block';
                 localStorage.setItem('highScore', yourScore)
@@ -79,9 +95,10 @@ function checkAnswer(currentLevel) {
 }
 
 function nextSequence() {
+    startBtn.setAttribute("style", "display: none !important;")
     userPattern = [];
     level++;
-    document.getElementById("level-title").textContent = `Level ${level}`;
+    levelTitle.textContent = `Level ${level}`;
     const randomColor = colors[Math.floor(Math.random() * 4)];
     gamePattern.push(randomColor);
 
